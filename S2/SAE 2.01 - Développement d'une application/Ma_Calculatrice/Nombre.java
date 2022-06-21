27f108ded37ef551cbeed4466611669785d30f5a
ClaudeZhang7 <98498100+ClaudeZhang7@users.noreply.github.com>
2022-06-22 00:03:16 +0200

    Add files via upload

---
+++ S2/SAE 2.01 - Développement d'une application/Ma_Calculatrice/Nombre.java
@@ -0,0 +1,26 @@
+
+public class Nombre{
+
+
+    private int valeurNombre;
+
+
+
+    public Nombre(int val){
+
+      this.valeurNombre = val;
+
+    }
+
+    public int valeur(){
+
+      return this.valeurNombre;
+
+    }
+
+    public String toString(){
+
+      return "Le nombre est : " + this.valeurNombre;
+
+    }
+}
